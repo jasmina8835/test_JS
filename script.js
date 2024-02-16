11d3dc40c2a2e29841cc6c271776873795932428
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:48:19 +0500

    test

---
+++ script.js
@@ -0,0 +1,84 @@
+// let carModel = "BMW"
+
+
+
+// let = result = 'I have car with model ${carModel}'
+
+// console.log(result);
+
+
+// ? Number
+
+// let a = 10;
+// let b = "qerty";
+
+// ? value as a string
+// console.log(a+b);
+
+// ? value as a number
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+
+// ? from string to number
+// console.log(Number(a));
+
+
+// ? from number to string
+// console.log("number converted to string"+ a.toString());
+
+
+// ? toFixed
+
+// let num = 45.3675789000
+
+// console.log(num);
+
+
+// document.write(num.toFixed(0))
+// ? cheksiz
+// console.log(infinity);
+
+// ? ildizdan chiqarish
+// console.log(Math.sqrt(num));
+
+
+
+
+
+
+
+
+// let a = 8;
+// let b = 5;
+
+// let result = Math.sqrt(Math.sqrt((a**3-b**3)+(a**3+b**3)))
+// console.log(result);
+
+
+
+// let c = 4;
+// let d = 9;
+
+// let result = (1/6 * Math.sqrt(c) + 1/3 * Math.sqrt(d))**2
+// console.log(result);
+
+
+
+
+// let a = 231;
+
+// let result = a.toString();
+// let first = result.slice(0,1).toString()
+// let second = result.slice(1,2).toString()
+// let third = result.slice(2,3).toString()
+
+// console.log(third+ second +first);
+
+
+
+
+
+
+
+
